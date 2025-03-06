290fd4af7eedcd6ef66008fce4552ea664f54e03
Eman Tariq <emantariq.1004@gmail.com>
2025-03-06 22:23:30 +0500

    orders page

---
+++ app/(store)/orders/page.tsx
@@ -0,0 +1,21 @@
+import { getMyOrders } from "@/sanity/lib/orders/getMyOrders"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
+async function Orders() {
+    const { userId } = await auth()
+
+    if (!userId) {
+        return redirect("/")
+    }
+
+    const orders = await getMyOrders(userId)
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Orders
